dca44f5beae4869fca9cdb3dad75dab1ec1ff0ed
scottmastro <scott-mastro@hotmail.com>
2026-04-18 02:09:46 -0400

    Remove stale PluginStore keys from old cache implementations
    Clean up 4 orphaned plugin_store_rows left behind by cache keys we've
    since replaced:
    
    - ebay_seller_weights  (old fetch_all_weights, replaced by ebay_ad_pool)
    - ebay_ad_seller_weights, ebay_ad_weight, ebay_ad_weights (earlier cache
      experiments, never referenced in current code)
    
    Only ebay_ad_pool remains in use.

---
+++ db/post_migrate/20260418000004_cleanup_stale_plugin_store_keys.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class CleanupStalePluginStoreKeys < ActiveRecord::Migration[7.0]
+  STALE_KEYS = %w[
+    ebay_seller_weights
+    ebay_ad_seller_weights
+    ebay_ad_weight
+    ebay_ad_weights
+  ].freeze
+
+  def up
+    execute <<~SQL
+      DELETE FROM plugin_store_rows
+      WHERE plugin_name = 'ebay_ad_plugin'
+        AND key IN (#{STALE_KEYS.map { |k| "'#{k}'" }.join(",")})
+    SQL
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
